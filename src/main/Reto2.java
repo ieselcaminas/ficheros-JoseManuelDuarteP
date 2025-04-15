03276ca677f5ad1e40988f606f9b14dca9fe6700
josduaper <josduaper@alu.edu.gva.es>
2025-04-15 12:05:19 +0200

    rqw3

--- src/main/Reto2.java
+++ src/main/Reto2.java
@@ -1,5 +1,26 @@
-public class Reto2 {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
+public class Reto2 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String opcion;
+
+        do {
+
+            System.out.println("Ingrese un anime (espacio para salir): ");
+            opcion = sc.nextLine();
+            String endpoint = "https://api.jikan.moe/v4/anime/" + opcion;
+
+            URL url = new URL(endpoint);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+
+
+        } while (true);
     }
 }
